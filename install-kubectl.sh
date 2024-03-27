6760b843f5b31c592ab83b3b656a611c9db1b82c
MubashirUsman <pink55651@gmail.com>
2024-03-27 17:27:48 +0100

    install kubeadm, kubectl, kubelet

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -13,3 +13,11 @@ sudo chmod +x /usr/local/bin/kubectl
 kubectl version --client
 
 echo "kubectl version $version was installed sucessfully"
+
+# We need to install three things: kubeadm, kubelet, kubectl
+
+sudo curl -L -o /usr/local/bin/kubeadm https://dl.k8s.io/release/$version/bin/linux/amd64/kubeadm
+sudo curl -L -o /usr/local/bin/kubelet https://dl.k8s.io/release/$version/bin/linux/amd64/kubelet
+sudo curl -L -o /usr/local/bin/kubectl https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl
+
+sudo chmod +x /usr/local/bin/kube*
